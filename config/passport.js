2061fd8e5e2a9938d0ebfa25aaa7df95ff7e554b
Mats Ljunggren <mljunggren@gmail.com>
2015-12-23 12:53:51 -0200

    Added working session functionality

---
+++ config/passport.js
@@ -0,0 +1,13 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Load the module dependencies
+var passport = require('passport'),
+	mongoose = require('mongoose');
+
+// Define the Passport configuration method
+module.exports = function() {
+	// Load the 'User' model
+	// var User = mongoose.model('User');
+	
+};
